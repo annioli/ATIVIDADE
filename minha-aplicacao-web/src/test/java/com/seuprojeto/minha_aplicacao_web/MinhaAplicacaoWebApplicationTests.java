30d845199b2e807d52c8e43adfce7839acbae9ce
Anilton Lima <aniltonlima4567@gmail.com>
2024-10-11 18:40:59 -0300

    primeiro commit

---
+++ minha-aplicacao-web/src/test/java/com/seuprojeto/minha_aplicacao_web/MinhaAplicacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.minha_aplicacao_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaAplicacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
